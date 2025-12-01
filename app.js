16924c2149ad533693dc6a7562ad28b37935855a
RealGenius1 <sptho24@gmail.com>
2025-12-01 16:17:46 -0500

    Set up my mongoDB
    it works now, basic background, must change

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 //jshint esversion:6
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -11,15 +7,6 @@ const _ = require("lodash");
 const { application } = require("express");
 const mongoose = require("mongoose");
 
-// var firebase = require("firebase/app");
-// require("firebase/auth");
-// require("firebase/firestore");
-
-
-
-// Initialize Firebase
-// const db = firebase.initializeApp(firebaseConfig);
-// const 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -32,7 +19,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://efecollins-admin:admin-efecollins-4311404114@cluster0.unuhgl6.mongodb.net/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://webmanage:<dbpassword>@events.1rpsho3.mongodb.net/?appName=Events", {useNewUrlParser: true});
 
 
 const postSchema = {
@@ -42,28 +29,28 @@ const postSchema = {
 
 
 const eventSchema = {
-  eventTitle: String,
   eventDescription: String,
-  eventImage: File,
+  eventImage: Number,
   eventLink: String,
   eventStart: String,
   eventEnd: String,
   eventLocation: String,
-  eventID: String
+  eventTitle: String,
 }
 
 
-const Post = mongoose.model("Post", postSchema);
+const Post = mongoose.model("events", eventSchema);
 
 app.get("/", (req, res) => {
   Post.find({}, (err, posts) => {
-    res.render("home", {homeStartingContent: homeStartingContent, posts: posts})
+    console.log(posts);
+    res.render("home", {homeStartingContent: homeStartingContent, posts: posts});
   })
 })
 
 app.get("/about", (req, res) => {
   res.render('about', {aboutContent: aboutContent})
-})
+})  
 
 app.get("/contact", (req, res) => {
   res.render('contact', {contactContent: contactContent})
@@ -76,8 +63,6 @@ app.get("/compose", (req, res) => {
 app.post("/compose", (req, res) => {
   //{postTitle: req.body.postTitle, postBody: req.body.postBody}
   const post = new Post({
-    postTitle: req.body.postTitle,
-    postBody: req.body.postBody
   })
   post.save(err => {
     if(!err) {
