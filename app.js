16ea6c4fd7c799fb7797847cf7e0314e45fa3a27
RealGenius1 <sptho24@gmail.com>
2025-12-07 19:44:50 -0500

    Lots
    1) Create compose page
    2) Linked to databse
    3) Set up dotenv
    4) Set up post page
    5) Changed how css is working
    6) Added images

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const { application } = require("express");
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+
+const db_password = process.env.DB_PASSWORD;
+
 
 const app = express();
 app.locals._ = _
@@ -19,7 +23,7 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-mongoose.connect("mongodb+srv://webmanage:<dbpassword>@events.1rpsho3.mongodb.net/?appName=Events", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://webmanage:" + db_password  + "@events.1rpsho3.mongodb.net/?appName=Events", {useNewUrlParser: true});
 
 
 const postSchema = {
@@ -63,6 +67,13 @@ app.get("/compose", (req, res) => {
 app.post("/compose", (req, res) => {
   //{postTitle: req.body.postTitle, postBody: req.body.postBody}
   const post = new Post({
+    eventTitle: req.body.eventTitle,
+    eventDescription: req.body.eventDescription,
+    eventImage: req.body.eventImage,
+    eventLink: req.body.eventLink,
+    eventStart: req.body.eventStart,
+    eventEnd: req.body.eventEnd,
+    eventLocation: req.body.eventLocation
   })
   post.save(err => {
     if(!err) {
@@ -74,7 +85,8 @@ app.post("/compose", (req, res) => {
 app.get("/posts/:postId", (req, res) => {
   const requestedId = _.toString(req.params.postId);
   Post.findOne({_id: requestedId}, (err, post) => {
-    res.render('post', {postTitle: post.postTitle, postBody: post.postBody})
+    console.log(post);
+    res.render('post', {post: post})
   })
 })
 
